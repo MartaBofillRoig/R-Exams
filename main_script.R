c18bb0356b4fd33e1bd032f0bd5b757c34ea0c20
MBR <marta.bofill.roig@upc.edu>
2020-10-15 10:24:41 +0200

    example

---
+++ main_script.R
@@ -0,0 +1,38 @@
+
+
+################################################################
+# SAMPLE EXAM using exams R package
+################################################################
+
+rm(list = ls())
+
+# install.packages('exams') 
+library("exams")
+
+setwd("C:/Users/mbofi/Dropbox/C5/Scripts/GitKraken/R-Exams")
+dir <- "C:/Users/mbofi/Dropbox/C5/Scripts/GitKraken/R-Exams"
+
+######################################################
+# Complete exam
+######################################################
+
+myexam <- c("capitals.Rmd","ttest.Rmd","tstat2")  
+# http://www.r-exams.org/templates/
+
+# marks
+marks <- c(2.5,2.5,5)
+
+
+set.seed(1)
+exams2nops(myexam, n = 4, 
+           # language = "en",
+           institution = "Universitat Politecnica de Catalunya",
+           title = "Statistical Inference",
+           dir = dir,
+           # name = "es", 
+           # date = "2019-05-30", startid = 1,
+           # logo = "logoub.png",blank=0, reglength=2, duplex=F,
+           points = marks , showpoints = TRUE)
+
+
+
